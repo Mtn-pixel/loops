00a4cb7942a491d7f816c0d71502d8092c6ad351
Mtn-pixel <76622528+Mtn-pixel@users.noreply.github.com>
2020-12-26 16:39:39 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
-    music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Forever)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Forever)
     basic.setLedColor(Colors.Orange)
     basic.showIcon(IconNames.Happy)
 })
